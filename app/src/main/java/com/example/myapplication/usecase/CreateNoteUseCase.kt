57bcd1dedb1c3f511281736bd1642343c08c47cd
weixu <xuwei19850423@gmail.com>
2025-03-05 10:38:36 +1100

    using rxjava to refactor insertNotes

--- app/src/main/java/com/example/myapplication/usecase/CreateNoteUseCase.kt
+++ app/src/main/java/com/example/myapplication/usecase/CreateNoteUseCase.kt
@@ -2,11 +2,13 @@ package com.example.myapplication.usecase
 
 import com.example.myapplication.dao.NoteDao
 import com.example.myapplication.entities.Note
+import io.reactivex.rxjava3.core.Completable
 import javax.inject.Inject
 
 class CreateNoteUseCase @Inject constructor(private val noteDao: NoteDao) {
 
-    fun insertNotes(vararg notes: Note) {
-        noteDao.insertAll(*notes)
+    fun insertNotes( note: Note): Completable {
+        return Completable
+            .fromCallable { noteDao.insertAll(note) }
     }
 }
